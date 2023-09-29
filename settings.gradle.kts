6f03c8c04f559c0d2b5f797eb8502ab7cc066695
mahendra-mous <hutamahendra830@gmail.com>
2023-09-29 14:48:28 +0700

    Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kartunama215410140"
+include(":app")
+ 
